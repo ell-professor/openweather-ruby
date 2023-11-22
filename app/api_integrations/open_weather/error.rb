9e8d395028cade6f11a7af07506482b93fc6b1d7
Professor <el_professor@gmail.com>
2023-11-22 22:54:02 +0530

    Add OpenWeather Client

---
+++ app/api_integrations/open_weather/error.rb
@@ -0,0 +1,15 @@
+module OpenWeather
+  class Error
+    CUSTOM_ERROR_MAP = {}
+
+    # Placeholder for custom errors
+    def self.map_error(e)
+      raise custom_error(response, e) || e
+    end
+
+    # TODO: Implement
+    def self.custom_error(e)
+      CUSTOM_ERROR_MAP[e]
+    end
+  end
+end
