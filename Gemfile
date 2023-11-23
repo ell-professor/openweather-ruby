8360a94729dc8774dd6141ce45a09d05643e51a1
Professor <el_professor@gmail.com>
2023-11-23 17:24:33 +0530

    Configure delayed job

--- Gemfile
+++ Gemfile
@@ -51,6 +51,7 @@ gem "bootsnap", require: false
 
 gem 'rest-client'
 gem 'dotenv-rails'
+gem 'delayed_job_active_record'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_22_130808) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_23_115302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
@@ -30,6 +30,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_22_130808) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["location_id"], name: "index_air_pollution_datapoints_on_location_id"
+  end
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string "locked_by"
+    t.string "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
   create_table "locations", force: :cascade do |t|
